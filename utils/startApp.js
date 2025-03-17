e76bcd5ae95b4992ab0e32a7763f9e23f5494e04
christophuff <chris.huff@hotmail.com>
2025-03-17 18:25:20 -0500

    huff-navbar and startApp function

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,4 +1,17 @@
-const startApp = () => {
+import logoutButton from '../components/logoutButton';
+import domBuilder from '../components/shared/domBuilder';
+import navBar from '../components/shared/navBar';
+import navEvents from '../components/events/navEventListeners';
+import domEvents from '../components/events/domEventListeners';
+import formEvents from '../components/events/formEventListeners';
+
+const startApp = (user) => {
+  domBuilder(user);
+  navBar(user);
+  logoutButton();
+  navEvents(user);
+  domEvents(user);
+  formEvents(user);
 };
 
 export default startApp;
