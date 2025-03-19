ccb62ac593f75eba1c90ccf4efbb0dbd21217e9d
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-03-18 20:30:29 -0400

    user can edit and delete orders

--- components/events/domEventListeners.js
+++ components/events/domEventListeners.js
@@ -1,5 +1,6 @@
-import { getOrders } from '../../api/ordersData';
+import { getOrders, deleteOrder, getSingleOrder } from '../../api/ordersData';
 import displayOrders from '../../pages/orders';
+import orderForm from '../forms/addOrderForm';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -8,6 +9,15 @@ const domEvents = () => {
     }
     if (e.target.id.includes('view-orders')) {
       getOrders().then(displayOrders);
+    }
+    if (e.target.id.includes('delete-order-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      deleteOrder(firebaseKey).then(getOrders).then(displayOrders);
+    }
+
+    if (e.target.id.includes('edit-order-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      getSingleOrder(firebaseKey).then((orderObj) => orderForm(orderObj));
     }
   });
 };
