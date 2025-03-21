0bb033bdf0fe0f3af87fcaec36caca6f6dc934ee
Duncan McPherson <duncan.mcpherson903@gmail.com>
2025-03-21 17:32:58 -0500

    Added some functionality to close order

--- components/events/domEventListeners.js
+++ components/events/domEventListeners.js
@@ -1,5 +1,8 @@
-import { getSingleItem } from '../../api/itemsData';
+import { getSingleItem, getItems, deleteItem } from '../../api/itemsData';
+import { getSingleOrder } from '../../api/ordersData';
 import addItemForm from '../forms/addItemForm';
+import closeOrderForm from '../forms/closeOrder';
+import showItems from '../../pages/items';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -16,6 +19,22 @@ const domEvents = () => {
     // Opens Form for adding items
     if (e.target.id.includes('add-item-btn')) {
       addItemForm();
+    }
+
+    // Opens Form for closing an order
+    if (e.target.id.includes('close-order-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      getSingleOrder(firebaseKey).then((orderObj) => closeOrderForm(orderObj));
+    }
+
+    if (e.target.id.includes('delete-item')) {
+      // eslint-disable-next-line no-alert
+      if (window.confirm('Want to delete?')) {
+        const [, firebaseKey] = e.target.id.split('--');
+        deleteItem(firebaseKey).then(() => {
+          getItems().then(showItems);
+        });
+      }
     }
   });
 };
